0354fcef10526595339a58b790c18e92d360004a
iDubrovsky <iapopko1@mts.ru>
2021-11-19 08:55:41 +0300

    [2l] Добавил необходимые файлы проекта.

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Илья on 19.11.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
