994f3b1bd66a0e0957d29ebdd1daee5354d10295
iDubrovsky <iapopko1@mts.ru>
2021-11-19 08:35:45 +0300

    [2l] Создал файл для домашнего задания ко 2-лекции.

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Илья on 19.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
