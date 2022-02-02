725c23f07e663c34a0fb1e2ff312467f29df9d90
hellodubrovsky <hellodubrovsky@icloud.com>
2022-02-02 10:55:28 +0300

    Создал проект для курсового проекта.

---
+++ MyHabits/MyHabits/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Илья on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
