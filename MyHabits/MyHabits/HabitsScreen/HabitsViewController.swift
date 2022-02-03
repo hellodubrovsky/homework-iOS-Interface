edd3472109de263558b95943acb68e2f44189d3d
hellodubrovsky <hellodubrovsky@icloud.com>
2022-02-03 18:20:22 +0300

    Добавил кнопку для перехода на экран создания привычки.

--- MyHabits/MyHabits/HabitsScreen/HabitsViewController.swift
+++ MyHabits/MyHabits/HabitsScreen/HabitsViewController.swift
@@ -6,5 +6,27 @@ class HabitsViewController: UIViewController {
         super.viewDidLoad()
         title = "Сегодня"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.rightBarButtonItem = addingHabitButton
+    }
+    
+    
+    
+    // MARK: Private objects
+    
+    private lazy var addingHabitButton: UIBarButtonItem = {
+        let largeFont = UIFont.systemFont(ofSize: 23)
+        let configuration = UIImage.SymbolConfiguration(font: largeFont)
+        let image = UIImage(systemName: "plus", withConfiguration: configuration)
+        let item = UIBarButtonItem(image: image, style: .done, target: self, action: #selector(showTheWindowForAddingNewHabit))
+        item.tintColor = UIColor(named: "purpleColorApp")
+        return item
+    }()
+    
+    
+    
+    // MARK: Private methods
+                             
+    @objc private func showTheWindowForAddingNewHabit() {
+        print("Pressed plus bar item")
     }
 }
